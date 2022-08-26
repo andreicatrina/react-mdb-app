0c54505d8eed0ff4b3c3cf69b1cd278e36825fa9
andreicatrina <Catrina.andrei93@yahoo.com>
2022-08-26 21:51:54 +0300

    Header styling, mobileMenu implemented + media queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,23 @@ import s from "./Header.module.css";
 import { FiHeart } from "react-icons/fi";
 import { HiSearch } from "react-icons/hi";
 import { RiShoppingBagLine } from "react-icons/ri";
-
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+import { BsArrowRightShort } from "react-icons/bs";
 import React from "react";
+import { useState } from "react";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function menuClick() {
+    if (showMenu === false) {
+      setShowMenu(true);
+    } else {
+      setShowMenu(false);
+    }
+  }
+
   return (
     <header>
       <section className={s.headerSection}>
@@ -15,13 +28,19 @@ const Header = () => {
               <div className={s.logoPic}>LOGO</div>
             </div>
           </div>
-          <div className={s.headerMenu}>
-            <a href="#">SHOP</a>
-            <a href="#">ABOUT US</a>
-            <a href="#">CONTACT</a>
+          <div className={s.centerMenuDiv}>
+            <div onClick={menuClick} className={s.hamburgerMenu}>
+              {showMenu === true ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </div>
+            <div className={s.headerMenu}>
+              <a href="#">SHOP</a>
+              <a href="#">ABOUT US</a>
+              <a href="#">CONTACT</a>
+            </div>
           </div>
+
           <div className={s.rightContainer}>
-            <a href="#">
+            <a className={s.addFavorites} href="#">
               <FiHeart />
             </a>
             <a href="#">
@@ -32,6 +51,16 @@ const Header = () => {
             </a>
           </div>
         </div>
+        {showMenu === true ? (
+          <div className={s.mobileMenu}>
+            <a className={s.shopMobile} href="#">
+              SHOP
+              <BsArrowRightShort />
+            </a>
+            <a href="#">ABOUT US</a>
+            <a href="#">CONTACT</a>
+          </div>
+        ) : null}
       </section>
     </header>
   );
